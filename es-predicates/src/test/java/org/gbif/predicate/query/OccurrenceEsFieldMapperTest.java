17bbebb0e8532908c037fd920817ed1a175cd5be
Nikolay Volik <nikolay.volik@hotmail.com>
2024-01-12 15:31:16 +0100

    gbif/pipelines#993 add dwc-api dependency

--- es-predicates/src/test/java/org/gbif/predicate/query/OccurrenceEsFieldMapperTest.java
+++ es-predicates/src/test/java/org/gbif/predicate/query/OccurrenceEsFieldMapperTest.java
@@ -1,11 +1,32 @@
 package org.gbif.predicate.query;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import org.gbif.api.model.occurrence.search.OccurrenceSearchParameter;
 import org.gbif.api.model.predicate.GreaterThanOrEqualsPredicate;
 import org.gbif.api.model.predicate.GreaterThanPredicate;
 import org.gbif.api.model.predicate.SimplePredicate;
 
 public class OccurrenceEsFieldMapperTest implements EsFieldMapper<OccurrenceSearchParameter> {
+
+  private static final Set<OccurrenceSearchParameter> VOCABULARY_SET =
+      new HashSet<>(
+          Arrays.asList(
+              OccurrenceSearchParameter.LIFE_STAGE,
+              OccurrenceSearchParameter.PATHWAY,
+              OccurrenceSearchParameter.DEGREE_OF_ESTABLISHMENT,
+              OccurrenceSearchParameter.ESTABLISHMENT_MEANS,
+              OccurrenceSearchParameter.EARLIEST_EON_OR_LOWEST_EONOTHEM,
+              OccurrenceSearchParameter.LATEST_EON_OR_HIGHEST_EONOTHEM,
+              OccurrenceSearchParameter.EARLIEST_ERA_OR_LOWEST_ERATHEM,
+              OccurrenceSearchParameter.LATEST_ERA_OR_HIGHEST_ERATHEM,
+              OccurrenceSearchParameter.EARLIEST_PERIOD_OR_LOWEST_SYSTEM,
+              OccurrenceSearchParameter.LATEST_PERIOD_OR_HIGHEST_SYSTEM,
+              OccurrenceSearchParameter.EARLIEST_EPOCH_OR_LOWEST_SERIES,
+              OccurrenceSearchParameter.LATEST_EPOCH_OR_HIGHEST_SERIES,
+              OccurrenceSearchParameter.EARLIEST_AGE_OR_LOWEST_STAGE,
+              OccurrenceSearchParameter.LATEST_AGE_OR_HIGHEST_STAGE));
 
   @Override
   public String getVerbatimFieldName(OccurrenceSearchParameter searchParameter) {
@@ -37,10 +58,7 @@ public class OccurrenceEsFieldMapperTest implements EsFieldMapper<OccurrenceSear
 
   @Override
   public boolean isVocabulary(OccurrenceSearchParameter searchParameter) {
-    return OccurrenceSearchParameter.LIFE_STAGE == searchParameter
-        || OccurrenceSearchParameter.PATHWAY == searchParameter
-        || OccurrenceSearchParameter.DEGREE_OF_ESTABLISHMENT == searchParameter
-        || OccurrenceSearchParameter.ESTABLISHMENT_MEANS == searchParameter;
+    return VOCABULARY_SET.contains(searchParameter);
   }
 
   @Override
