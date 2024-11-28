829f4a5c283776709b7b174b59755f15cb5b6350
gbif-jenkins <dev@gbif.org>
2024-11-28 13:17:15 +0000

    Publishing Site Docs for predicates-parent:1.3.3-SNAPSHOT

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.gbif.predicate.query"},{"l":"org.gbif.predicate.query.occurrence"}]
---
+++ testapidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.gbif.predicate.query","l":"EsQueryVisitorTest"},{"p":"org.gbif.predicate.query","l":"OccurrenceEsFieldMapperTest"},{"p":"org.gbif.predicate.query","l":"SQLQueryVisitorTest"}]
