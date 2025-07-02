fdd08485daf075ae2b210db73da5dede1e8880e8
gbif-jenkins <dev@gbif.org>
2025-07-02 12:00:38 +0000

    Publishing Site Docs for predicates-parent:2.1.0-SNAPSHOT

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.gbif.predicate.query"},{"l":"org.gbif.predicate.query.occurrence"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.gbif.predicate.query"},{"l":"org.gbif.predicate.query.occurrence"}]
--- testapidocs/type-search-index.js
+++ testapidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.gbif.predicate.query","l":"EsQueryVisitorTest"},{"p":"org.gbif.predicate.query","l":"OccurrenceEsFieldMapperTest"},{"p":"org.gbif.predicate.query","l":"SQLQueryVisitorTest"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.gbif.predicate.query","l":"EsQueryVisitorTest"},{"p":"org.gbif.predicate.query","l":"OccurrenceEsFieldMapperTest"},{"p":"org.gbif.predicate.query","l":"SQLQueryVisitorTest"}]
